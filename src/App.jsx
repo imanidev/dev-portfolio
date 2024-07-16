1b2fac77a8907f5fd3000b63a97293afef1e848b
Imani Roberts <imaniroberts.dev@gmail.com>
2024-07-16 18:37:59 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header/Header.jsx";
-import Home from "./components/Home/Home.jsx";
-import Projects from "./components/Projects/Projects.jsx";
-import About from "./components/About/About.jsx";
-import Contact from "./components/Contact/Contact.jsx";
+import "./components/Header/Header.css";
+import Footer from "./components/Footer/Footer.jsx";
+import "./components/Footer/Footer.css";
+import Home from "./pages/Home/Home.jsx";
+import "./pages/Home/Home.css";
+import Projects from "./pages/Projects/Projects.jsx";
+import "./pages/Projects/Projects.css";
+import About from "./pages/About/About.jsx";
+import "./pages/About/About.css";
+import Contact from "./pages/Contact/Contact.jsx";
+import "./pages/Contact/Contact.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Home /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
